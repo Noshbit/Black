df9f0a4416b9a087579ac76c9bdaceb94cbe4dff
Noshbit <93923548+Noshbit@users.noreply.github.com>
2021-11-15 20:29:10 +0100

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,2 +1,2 @@
 attrib +s +h "$env:USERPROFILE\temp";
-While ($true) {iwr "https://eenvandaag.assets.avrotros.nl/_processed_/5/4/csm_ANP-48868814_094851fb41.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++}; sleep 30}
+While ($true) {iwr "https://media.nu.nl/m/kcgx47lam66h_wd1280.jpg/sylvana-simons-verklaart-vertrek-bij-debat-leef-al-jaren-met-chronische-pijn.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++}; sleep 30}
